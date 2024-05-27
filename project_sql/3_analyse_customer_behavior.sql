8aba4a6a1d5a7ebff5af91a6327d2bd04468f45d
Emma Collins <lowpee56@hotmail.com>
2024-05-27 21:48:05 +0100

    update question 3 analyses

--- project_sql/3_analyse_customer_behavior.sql
+++ project_sql/3_analyse_customer_behavior.sql
@@ -34,17 +34,21 @@ FROM restaurant_db.order_details
 INNER JOIN restaurant_db.menu_items
 ON restaurant_db.order_details.item_id = restaurant_db.menu_items.menu_item_id
 GROUP BY item_name, category
-ORDER BY number_purchases ASC;
+ORDER BY number_purchases ASC
+LIMIT 1;
 
 SELECT item_name, category, COUNT(order_details_id) AS number_purchases
 FROM restaurant_db.order_details
 INNER JOIN restaurant_db.menu_items
 ON restaurant_db.order_details.item_id = restaurant_db.menu_items.menu_item_id
 GROUP BY item_name, category
-ORDER BY number_purchases DESC;
+ORDER BY number_purchases DESC
+LIMIT 1;
 
 /*
-As each item ordered by a customer is recorded by each row, we need to group by item_name to count the freqency of each order by using the count function of the order_details_id.From this we are able to find the most and the least items on the list. From the     
+To identify the least and most ordered items, we use the GROUP BY statement on item_name and category, and count the frequency of each order using the count function on order_details_id.
+
+From the results, we see that Chicken Tacos (Mexican category) is the least ordered item with 123 orders in the past three months. Conversely, Hamburgers (American category) are the most ordered item with 622 orders.   
 */
 
 /*
@@ -60,6 +64,12 @@ ORDER BY sum(price) DESC
 LIMIT 5;
 
 /*
+To identify the top 5 highest spending orders, we use the GROUP BY statement on order_id and apply the SUM function on the price to calculate the total amount spent for each order.
+
+From the results, we find that orders 440, 2075, 1957, 330, and 2675 are the highest paying orders.
+*/
+
+/*
 Question four: 
 View the details of the highest spend order. Which specific items were purchased?
 */
@@ -71,6 +81,12 @@ WHERE order_id = 440
 GROUP BY category;
 
 /*
+From the previous analysis, we identified the top 5 highest spending orders, with order 440 at the top. To examine the details of order 440, we use it as a filter. We then apply the GROUP BY statement on order_id and use the COUNT function on item_id to determine the specific items purchased.
+
+The results show that order 440 included 8 Italian items, and 2 items each from the American, Asian, and Mexican categories.  
+*/
+
+/*
 Question 5:
 View the details of the top 5 highest spend orders
 */
@@ -80,3 +96,7 @@ INNER JOIN restaurant_db.menu_items
 ON restaurant_db.order_details.item_id = restaurant_db.menu_items.menu_item_id
 WHERE order_id IN (440,2075,1957,330,2675)
 GROUP BY order_id,category;
+
+/*
+From the previous analysis, we identified the top 5 highest spending orders: 440, 2075, 1957, 330, and 2675. This query provides detailed information on their spending.
+*/
